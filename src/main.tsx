319d56144462600e347d7f925c08a3a35427d268
elencodes <playnonstop205@gmail.com>
2025-04-06 15:03:00 +0400

    feat(app): set up the ChakraProvider at the root of application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from '~/app/App.tsx';
-import { store } from '~/store/configure-store.ts';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
+        <ChakraProvider>
             <App />
-        </Provider>
+        </ChakraProvider>
     </StrictMode>,
 );
