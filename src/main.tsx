25573d3c9d4cbc6c29a82817e7bafac900b3c256
elencodes <playnonstop205@gmail.com>
2025-04-06 20:56:00 +0400

    feat(theme): connecting mainTheme to the project

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,12 @@ import { Provider } from 'react-redux';
 
 import { App } from './app/App';
 import { store } from './store/configure-store';
+import mainTheme from './theme';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <ChakraProvider>
+            <ChakraProvider theme={mainTheme}>
                 <App />
             </ChakraProvider>
         </Provider>
