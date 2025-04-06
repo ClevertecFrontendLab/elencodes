262d6f4672a53d446214796b9849f09bf62ca10d
elencodes <playnonstop205@gmail.com>
2025-04-06 19:04:30 +0400

    fix(app): add header display

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from '~/app/App.tsx';
+import { App } from './app/App';
+import { store } from './store/configure-store';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
+        <Provider store={store}>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </Provider>
     </StrictMode>,
 );
