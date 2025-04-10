fc67d76eed94e069542f026c706cb8710d1abc56
elencodes <playnonstop205@gmail.com>
2025-04-10 20:30:21 +0400

    refactor(type): delete type of actionroundbutton

--- src/types/ActionRoundButtonTypes.ts
+++
@@ -1,9 +0,0 @@
-import { IconProps } from '@chakra-ui/icons';
-import { ComponentWithAs } from '@chakra-ui/react';
-
-export type ActionRoundButtonProps = {
-    ariaLabel: string;
-    icon: ComponentWithAs<'svg', IconProps>;
-    boxSize?: string;
-    text?: string;
-};
