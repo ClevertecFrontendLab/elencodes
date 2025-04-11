a5f524aec27bf31091c914e51b94fa291dc5c748
elencodes <playnonstop205@gmail.com>
2025-04-11 17:38:27 +0400

    feat(main): add the main section with routing for the home page

---
+++ src/layout/Main/Main.style.ts
@@ -0,0 +1,7 @@
+import { SystemStyleObject } from '@chakra-ui/react';
+
+export const mainContainerStyles: SystemStyleObject = {
+    maxWidth: '100%',
+    px: { base: '16px', md: '20px', lg: '280px' },
+    pb: { base: '68px', lg: 0 },
+};
---
+++ src/layout/Main/Main.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/icons';
+import { Container } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router';
+
+import { HomePage } from '../pages/HomePage/HomePage';
+import { mainContainerStyles } from './Main.style';
+
+export const Main = () => (
+    <Box as='main'>
+        <Container sx={mainContainerStyles}>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+            </Routes>
+        </Container>
+    </Box>
+);
