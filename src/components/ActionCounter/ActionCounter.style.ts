3161e9f18a2d3b216bdb87286dbe2c43bdf8823f
elencodes <playnonstop205@gmail.com>
2025-04-11 17:07:56 +0400

    fix(counter): fix styles for the action counter

--- src/components/ActionCounter/ActionCounter.style.ts
+++ src/components/ActionCounter/ActionCounter.style.ts
@@ -1,7 +1,6 @@
 import { SystemStyleObject } from '@chakra-ui/react';
 
 export const actionCounterTextStyles: SystemStyleObject = {
-    fontSize: { base: '12', lg: '16' },
     fontWeight: 'semibold',
     lineHeight: { base: '133%', lg: '150%' },
     color: 'lime.600',
--- src/components/ActionCounter/ActionCounter.tsx
+++ src/components/ActionCounter/ActionCounter.tsx
@@ -5,10 +5,16 @@ import { ActionCounterPropsType } from '~/types/ActionCounterType';
 
 import { actionCounterTextStyles } from './ActionCounter.style';
 
-export const ActionCounter = ({ countClick, icon, spacing, padding }: ActionCounterPropsType) => (
+export const ActionCounter = ({
+    countClick,
+    icon,
+    spacing,
+    padding,
+    fontSize = 'xs',
+}: ActionCounterPropsType) => (
     <HStack spacing={spacing} p={padding}>
         <Icon as={icon} />
-        <Text as='span' sx={actionCounterTextStyles}>
+        <Text as='span' fontSize={fontSize} sx={actionCounterTextStyles}>
             {countClick}
         </Text>
     </HStack>
