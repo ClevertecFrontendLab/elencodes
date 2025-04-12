22f4d683b502e8ee24bf173cb98218ae31710055
elencodes <playnonstop205@gmail.com>
2025-04-12 17:42:43 +0400

    feat(button): add the save button for the horizontal cards

---
+++ src/components/Buttons/SaveButton/SaveButton.styles.ts
@@ -0,0 +1,8 @@
+import { SystemStyleObject } from '@chakra-ui/react';
+
+export const saveButtonStyles: SystemStyleObject = {
+    borderColor: 'blackAlpha.600',
+    '& span': {
+        marginRight: { base: 0, lg: 2 },
+    },
+};
---
+++ src/components/Buttons/SaveButton/SaveButton.tsx
@@ -0,0 +1,18 @@
+import { Show } from '@chakra-ui/icons';
+import { Button } from '@chakra-ui/react';
+
+import BookmarkIcon from '~/icons/CounterIcons/BookmarkIcon';
+
+import { saveButtonStyles } from './SaveButton.styles';
+
+export const SaveButton = () => (
+    <Button
+        colorScheme='black'
+        size={{ base: 'xs', lg: 'sm' }}
+        variant='outline'
+        leftIcon={<BookmarkIcon />}
+        sx={saveButtonStyles}
+    >
+        <Show above='lg'>Сохранить</Show>
+    </Button>
+);
