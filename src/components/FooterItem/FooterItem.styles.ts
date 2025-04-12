e57b006aa29c94db0f9d3ada66515f907f875556
elencodes <playnonstop205@gmail.com>
2025-04-12 22:31:31 +0400

    feat(footeritem): add items for the footer section

---
+++ src/components/FooterItem/FooterItem.styles.ts
@@ -0,0 +1,26 @@
+import { SystemStyleObject } from '@chakra-ui/react';
+
+export const footerWrapperStyles: SystemStyleObject = {
+    h: '100%',
+    w: '100%',
+    cursor: 'pointer',
+};
+
+export const footerInnerWrapperStyles: SystemStyleObject = {
+    h: '100%',
+    w: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+
+export const footerIconsBoxStyles: SystemStyleObject = {
+    h: '40px',
+    w: '40px%',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+
+export const footerTextStyles: SystemStyleObject = {
+    fontSize: '12',
+    lineHeight: '133%',
+};
