4fafa6e463a824d25a26849e2017540ee7ae7b9a
elencodes <playnonstop205@gmail.com>
2025-04-12 18:01:28 +0400

    feat(page): add the home page

--- src/layout/pages/HomePage/HomePage.tsx
+++ src/layout/pages/HomePage/HomePage.tsx
@@ -1,9 +1,32 @@
-import { SearchingContent } from '~/components/SearchingContent/SearchingContent';
-import { NewRecipesSection } from '~/layout/Main/Sections/newRecipes/NewRecipesSection';
+import { useNavigate } from 'react-router';
 
-export const HomePage = () => (
-    <>
-        <SearchingContent title='Приятного аппетита!' />
-        <NewRecipesSection />
-    </>
-);
+import { TitleSectionWithButton } from '~/components/Buttons/TitleSectionWithButton/TitleSectionWithButton';
+import { SearchingContent } from '~/components/SearchingContent/SearchingContent';
+import { theJuciestCardsData } from '~/data/homePage/theJuciestCardsData';
+import { NewRecipesSection } from '~/layout/Main/Sections/newRecipes/NewRecipesSection';
+import { MainSection } from '~/layout/Main/Sections/theJuciest/MainSection';
+
+export const HomePage = () => {
+    const navigate = useNavigate();
+
+    const showTheJuciest = () => {
+        navigate('/juciest');
+    };
+
+    return (
+        <>
+            <SearchingContent title='Приятного аппетита!' />
+            <NewRecipesSection />
+            <TitleSectionWithButton onClick={showTheJuciest} />
+            <MainSection
+                buttonTitle='Вся подборка'
+                sizeButton='md'
+                displayButton={{ base: 'flex', lg: 'none' }}
+                mainSectionData={theJuciestCardsData}
+                showRightIcon={true}
+                onClick={showTheJuciest}
+                dataTestId='juiciest-link-mobile'
+            />
+        </>
+    );
+};
