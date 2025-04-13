c197084f3b7c7abbd921549852f433d2106dfb68
elencodes <playnonstop205@gmail.com>
2025-04-13 13:27:40 +0400

    feat(section): add the last section on the home page

--- src/layout/pages/HomePage/HomePage.tsx
+++ src/layout/pages/HomePage/HomePage.tsx
@@ -3,7 +3,10 @@ import { useNavigate } from 'react-router';
 import { TitleSectionWithButton } from '~/components/Buttons/TitleSectionWithButton/TitleSectionWithButton';
 import { SearchingContent } from '~/components/SearchingContent/SearchingContent';
 import { theJuciestCardsData } from '~/data/homePage/theJuciestCardsData';
+import { veganCuisineData } from '~/data/homePage/veganCuisineData';
+import { veganCuisineDataShort } from '~/data/homePage/veganCuisineDataShort';
 import { CulinaryBlogs } from '~/layout/Main/Sections/culinaryBlogs/CulinaryBlogs';
+import { LastSection } from '~/layout/Main/Sections/lastSection/LastSection';
 import { NewRecipesSection } from '~/layout/Main/Sections/newRecipes/NewRecipesSection';
 import { MainSection } from '~/layout/Main/Sections/theJuciest/MainSection';
 
@@ -29,6 +32,12 @@ export const HomePage = () => {
                 dataTestId='juiciest-link-mobile'
             />
             <CulinaryBlogs />
+            <LastSection
+                title='Веганская кухня'
+                text='Интересны не только убеждённым вегетарианцам, но и тем, кто хочет попробовать вегетарианскую диету и готовить вкусные вегетарианские блюда.'
+                firstCardsData={veganCuisineData}
+                secondCardsData={veganCuisineDataShort}
+            />
         </>
     );
 };
