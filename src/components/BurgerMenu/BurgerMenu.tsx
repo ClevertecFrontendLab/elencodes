20129e3086a5024cb98e0d2c5d86c35a3b1b5669
elencodes <playnonstop205@gmail.com>
2025-04-28 00:29:51 +0400

    feat(burger): add the burger menu component

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,71 @@
+import { Box, CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { IconButton, useDisclosure } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
+import { Header } from '~/layout/Header/Header';
+import { NavMenu } from '~/layout/NavMenu/NavMenu';
+
+import { BreadCrumbs } from '../BreadCrumbs/BreadCrumbs';
+
+export const BurgerMenu = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    useEffect(() => {
+        isOpen && (document.body.style.overflow = 'hidden');
+        !isOpen && (document.body.style.overflow = 'unset');
+    }, [isOpen]);
+
+    return (
+        <Box display={{ base: 'block', lg: 'none' }}>
+            {isOpen && (
+                <Box
+                    onClick={onClose}
+                    bg='blackAlpha.300'
+                    pos='fixed'
+                    top={0}
+                    bottom={0}
+                    left={0}
+                    right={0}
+                    zIndex={19}
+                />
+            )}
+            {!isOpen && (
+                <IconButton
+                    data-test-id='hamburger-icon'
+                    aria-label='Open menu'
+                    icon={<HamburgerIcon />}
+                    onClick={onOpen}
+                    display={{ base: 'flex', xl: 'none' }}
+                    variant='ghost'
+                    size='lg'
+                    colorScheme='gray'
+                    mr={{ sm: '16px', md: '20px' }}
+                />
+            )}
+            {isOpen && (
+                <>
+                    <Box w='344px' pos='relative'>
+                        <IconButton
+                            data-test-id='close-icon'
+                            pos='fixed'
+                            top='8px'
+                            right='16px'
+                            aria-label='Close menu'
+                            icon={<CloseIcon />}
+                            onClick={onClose}
+                            variant='ghost'
+                            size='lg'
+                            zIndex={101}
+                        />
+                        <Header isBurgerMenu={true} />
+                        <NavMenu isBurgerMenu={true} closeBurgerMenu={onClose}>
+                            <Box pos='relative' zIndex={101} p='16px 20px 0 20px'>
+                                <BreadCrumbs onBreadcrumbItemClick={onClose} />
+                            </Box>
+                        </NavMenu>
+                    </Box>
+                </>
+            )}
+        </Box>
+    );
+};
--- src/components/Buttons/BurgerButton/BurgerButton.styles.ts
+++
@@ -1,8 +0,0 @@
-import { SystemStyleObject } from '@chakra-ui/react';
-
-export const burgerButtonStyles: SystemStyleObject = {
-    display: { base: 'flex', lg: 'none' },
-    w: '48px',
-    h: '48px',
-    bg: 'unset',
-};
