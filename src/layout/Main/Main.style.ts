95d57463a364c8fa1b8ee4dbb86f912b13a4a8bb
elencodes <playnonstop205@gmail.com>
2025-04-28 01:22:57 +0400

    fix(main): fix the main component

--- src/layout/Main/Main.style.ts
+++ src/layout/Main/Main.style.ts
@@ -4,4 +4,5 @@ export const mainContainerStyles: SystemStyleObject = {
     maxWidth: '100%',
     px: { base: '16px', md: '20px', lg: '280px' },
     pb: { base: '68px', lg: 0 },
+    mt: { base: '64px', lg: '80px' },
 };
