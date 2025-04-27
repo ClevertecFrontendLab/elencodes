91ddb60fba134a3144eb2bf37ee2fdbda3eb277c
elencodes <playnonstop205@gmail.com>
2025-04-28 00:49:16 +0400

    fix(data): fix data for the last section

--- src/data/veganCuisinePage/dessertsData.ts
+++ src/data/veganCuisinePage/dessertsData.ts
@@ -1,31 +1,30 @@
 import { BookmarkIcon } from '~/icons/counterIcons/BookmarkIcon';
 import ReactionIcon from '~/icons/counterIcons/ReactionIcon';
-import KidsCoursesIcon from '~/icons/NavMenuIcons/KidsCoursesIcon';
 import { FirstCardsDataType } from '~/types/FirstCardsDataType';
 
 export const dessertsData: FirstCardsDataType[] = [
     {
         id: 1,
         title: 'Бананово-молочное желе',
-        text: 'Молочное желе – это просто, вкусно и полезно, ведь для его приготовления в качестве основы используется молоко.',
+        description:
+            'Молочное желе – это просто, вкусно и полезно, ведь для его приготовления в качестве основы используется молоко.',
         iconCounters: [
             { icon: BookmarkIcon, countClick: 1, spacing: '6px', padding: '4px' },
             { icon: ReactionIcon, countClick: 1, spacing: '6px', padding: '4px' },
         ],
-        tagIcon: KidsCoursesIcon,
-        tagTitle: 'Детские блюда',
+        category: 'kids-dish',
         bgColorTag: 'lime.50',
     },
     {
         id: 2,
         title: 'Нежный сливочно-сырный крем для кексов',
-        text: 'Сливочно-сырным кремом можно украсить кексы, либо другую выпечку, а также этим кремом можно наполнить заварные пирожные.',
+        description:
+            'Сливочно-сырным кремом можно украсить кексы, либо другую выпечку, а также этим кремом можно наполнить заварные пирожные.',
         iconCounters: [
             { icon: BookmarkIcon, countClick: 2, spacing: '6px', padding: '4px' },
             { icon: ReactionIcon, countClick: 1, spacing: '6px', padding: '4px' },
         ],
-        tagIcon: KidsCoursesIcon,
-        tagTitle: 'Детские блюда',
+        category: 'kids-dish',
         bgColorTag: 'lime.50',
     },
 ];
