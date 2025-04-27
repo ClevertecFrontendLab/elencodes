7e966aa86bbfc5f4bcba334c79469e3ebdb5b87e
elencodes <playnonstop205@gmail.com>
2025-04-28 01:14:58 +0400

    fix(styles): fix styles for the new recipe section

--- src/layout/Main/Sections/newRecipes/NewRecipesSection.tsx
+++ src/layout/Main/Sections/newRecipes/NewRecipesSection.tsx
@@ -1,40 +1,14 @@
-import { Box, Show } from '@chakra-ui/icons';
-import { Flex, IconButton } from '@chakra-ui/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-import { NewRecipesCard } from '~/components/Cards/NewRecipesCard/NewRecipesCard';
+import { Box } from '@chakra-ui/icons';
+
 import { SectionTitle } from '~/components/SectionTitle/SectionTitle';
-import { newRecipesCardsData } from '~/data/homePage/newRecipesCardsData';
-import ArrowLeftIcon from '~/icons/SwipeSectionIcons/ArrowLeftIcon';
-import ArrowRightIcon from '~/icons/SwipeSectionIcons/ArrowRightIcon';
-
-import {
-    cardBoxStyles,
-    newRecipesSectionStyles,
-    sectionArrowLeftStyles,
-    sectionArrowRightStyles,
-} from './NewRecipesSection.style';
+import { Slider } from '~/components/Slider/Slider';
 
 export const NewRecipesSection = () => (
     <Box as='section'>
         <SectionTitle title='Новые рецепты' />
-        <Box sx={newRecipesSectionStyles}>
-            <Flex sx={cardBoxStyles}>
-                {newRecipesCardsData.map((el) => (
-                    <NewRecipesCard key={el.id} {...el} />
-                ))}
-            </Flex>
-            <Show above='lg'>
-                <IconButton
-                    aria-label='Arrow Left'
-                    icon={<ArrowLeftIcon boxSize={{ base: '20px', xxl: '24px' }} />}
-                    sx={sectionArrowLeftStyles}
-                />
-                <IconButton
-                    aria-label='Arrow Right'
-                    icon={<ArrowRightIcon boxSize={{ base: '20px', xxl: '24px' }} />}
-                    sx={sectionArrowRightStyles}
-                />
-            </Show>
-        </Box>
+        <Slider />
     </Box>
 );
