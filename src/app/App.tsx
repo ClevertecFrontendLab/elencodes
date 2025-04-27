c5f5792c29c9722e7fc535f306c0fc28eadca7f0
elencodes <playnonstop205@gmail.com>
2025-04-28 01:15:46 +0400

    fix(app): fix app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import '/src/styles/fonts.css';
 import { Show } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { ScrollToTop } from '~/components/ScrollToTop/ScrollToTop';
 import { Footer } from '~/layout/Footer/Footer';
 import { Header } from '~/layout/Header/Header';
 import { Main } from '~/layout/Main/Main';
@@ -12,14 +13,13 @@ import mainTheme from '~/styles/theme';
 
 export const App: React.FC = () => (
     <ChakraProvider theme={mainTheme}>
+        <ScrollToTop />
         <Header />
         <Main />
         <Show above='lg'>
             <NavMenu />
             <SideBar />
         </Show>
-        <Show below='lg'>
-            <Footer />
-        </Show>
+        <Footer />
     </ChakraProvider>
 );
