90789df8b866306dfa7a2ea3cac4116e9df64939
elencodes <playnonstop205@gmail.com>
2025-04-28 00:45:44 +0400

    fix(styles): fix styles for the searchingcontent component

--- src/components/SearchingContent/SearchingContent.styles.ts
+++ src/components/SearchingContent/SearchingContent.styles.ts
@@ -1,11 +1,11 @@
 import { SystemStyleObject } from '@chakra-ui/react';
 
-export const HeaderPageContentStyles: SystemStyleObject = {
-    flexDirection: 'column',
-    alignItems: 'center',
-    pt: { base: '16px', lg: '32px' },
-    mb: { base: '32px', lg: '56px' },
-};
+// export const HeaderPageContentStyles: SystemStyleObject = {
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     pt: { base: '16px', lg: '32px' },
+//     mb: { base: '32px', lg: '56px' },
+// };
 
 export const PageTitleStyles: SystemStyleObject = {
     fontSize: { base: '24', lg: '48' },
@@ -30,12 +30,12 @@ export const SearchingBoxStyles: SystemStyleObject = {
     mb: { base: 0, lg: '16px' },
 };
 
-export const FilterButtonStyles: SystemStyleObject = {
-    variant: 'outline',
-    boxSize: { base: '32px', lg: '48px' },
-    border: '1px solid rgba(0, 0, 0, 0.48)',
-    bg: 'transparent',
-};
+// export const FilterButtonStyles: SystemStyleObject = {
+//     variant: 'outline',
+//     boxSize: { base: '32px', lg: '48px' },
+//     border: '1px solid rgba(0, 0, 0, 0.48)',
+//     bg: 'transparent',
+// };
 
 export const SearchingInputBoxStyles: SystemStyleObject = {
     flex: '1',
@@ -43,39 +43,39 @@ export const SearchingInputBoxStyles: SystemStyleObject = {
     alignItems: 'center',
 };
 
-export const SearchingInputStyles: SystemStyleObject = {
-    width: '100%',
-    height: '100%',
-    fontSize: { base: '14', lg: '18' },
-    border: '1px solid rgba(0, 0, 0, 0.48)',
-    _placeholder: { color: 'lime.800' },
-};
+// export const SearchingInputStyles: SystemStyleObject = {
+//     width: '100%',
+//     height: '100%',
+//     fontSize: { base: '14', lg: '18' },
+//     border: '1px solid rgba(0, 0, 0, 0.48)',
+//     _placeholder: { color: 'lime.800' },
+// };
 
-export const SearchingIconBoxStyles: SystemStyleObject = {
-    w: { base: '32px', lg: '48px' },
-    h: { base: '32px', lg: '48px' },
-    cursor: 'pointer',
-};
+// export const SearchingIconBoxStyles: SystemStyleObject = {
+//     w: { base: '32px', lg: '48px' },
+//     h: { base: '32px', lg: '48px' },
+//     cursor: 'pointer',
+// };
 
-export const FormControlBoxStyles: SystemStyleObject = {
-    minW: '518px',
-    gap: '16px',
-};
+// export const FormControlBoxStyles: SystemStyleObject = {
+//     minW: '518px',
+//     gap: '16px',
+// };
 
-export const FormControlStyles: SystemStyleObject = {
-    display: 'flex',
-    alignItems: 'center',
-    flexGrow: '1',
-    justifyContent: 'flex-end',
-};
+// export const FormControlStyles: SystemStyleObject = {
+//     display: 'flex',
+//     alignItems: 'center',
+//     flexGrow: '1',
+//     justifyContent: 'flex-end',
+// };
 
-export const FormLabelStyles: SystemStyleObject = {
-    mb: '0',
-    fontSize: '16',
-    fontWeight: 'medium',
-};
+// export const FormLabelStyles: SystemStyleObject = {
+//     mb: '0',
+//     fontSize: '16',
+//     fontWeight: 'medium',
+// };
 
-export const FormSelectStyles: SystemStyleObject = {
-    border: '1px solid rgba(0, 0, 0, 0.08)',
-    color: 'blackAlpha.700',
-};
+// export const FormSelectStyles: SystemStyleObject = {
+//     border: '1px solid rgba(0, 0, 0, 0.08)',
+//     color: 'blackAlpha.700',
+// };
