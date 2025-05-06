ccadf08149a5a10b192f9d3749ca3c9707c53da6
elencodes <playnonstop205@gmail.com>
2025-05-06 23:34:45 +0400

    fix(types): change types for value Item

--- src/model/filterSlice.ts
+++ src/model/filterSlice.ts
@@ -3,6 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type PrimitiveItem = string | number;
 interface ValueItem {
     value: string;
+    label: string;
 }
 type ToggleableItem = PrimitiveItem | ValueItem;
 
