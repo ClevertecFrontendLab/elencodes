d8be63a6516cdecafa17fdc9aef850613fdd59b2
elencodes <playnonstop205@gmail.com>
2025-05-07 23:38:54 +0400

    fix(types): change types

--- src/types/MenuItemTagType.ts
+++
@@ -1,9 +0,0 @@
-import { IconProps } from '@chakra-ui/icons';
-import { ComponentWithAs } from '@chakra-ui/react';
-
-export type MenuItemTagPropsType = {
-    bgColor: string;
-    category?: string | string[];
-    icon?: ComponentWithAs<'svg', IconProps>;
-    title?: string;
-};
--- src/types/SecondCardsDataType.ts
+++
@@ -1,8 +0,0 @@
-import { IconProps } from '@chakra-ui/icons';
-import { ComponentWithAs } from '@chakra-ui/react';
-
-export type SecondCardsDataType = {
-    id: number;
-    icon: ComponentWithAs<'svg', IconProps>;
-    title: string;
-};
