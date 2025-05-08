826b4f442cc67ffc99e4a46f89bfe3987872b4f9
elencodes <playnonstop205@gmail.com>
2025-05-08 13:25:23 +0400

    fix(hooks): fix screen size

--- src/hooks/use-screen-size.tsx
+++ src/hooks/use-screen-size.tsx
@@ -1,8 +1,8 @@
 import { useMediaQuery } from '@chakra-ui/react';
 
 export const useScreenSize = () => {
-    const [isTablet] = useMediaQuery('(max-width: 768px)');
-    const [isMobile] = useMediaQuery('(max-width: 360px)');
+    const [isTablet] = useMediaQuery('(max-width: 1440px)');
+    const [isMobile] = useMediaQuery('(max-width: 767px)');
 
     return { isTablet, isMobile };
 };
